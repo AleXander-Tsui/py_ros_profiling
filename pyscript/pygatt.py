80de0a7f969c9e1bebe6a4b8235afc0597e0c918
Alexander <xyfthu16@gmail.com>
2019-05-13 20:49:52 +0800

    add overlap check

--- pyscript/pygatt.py
+++ pyscript/pygatt.py
@@ -1,5 +1,7 @@
 import numpy as np
+import json 
 import sys
+import pdb
 
 def get_times(filein, actionname, id, colorname):
     time_stamps = []
@@ -30,7 +32,38 @@ def print_outinfo(outputinfo,minstart=0,file=sys.stdout):
         else :
             print( '{{ "name" : "{action}", "value" : [{id}, {start}, {end}, {duration} ], "itemStyle":{{"normal": {{"color": {color} }} }}  }}'.format(**info ), file=file )
     print(']',file=file)
-    
+
+
+def overlaped(query, match):
+    # print(query)
+    # print(match)
+    if query['start'] < match['start'] and query['end'] > match['start']:
+        return True
+    if query['start'] >= match['start'] and query['start'] <= match['end']:
+        return True
+    return False
+        
+
+def valid_json(jsonData, line_num = 3, nonoverlap_id=[2]):
+    items = json.loads(jsonData)
+    # pdb.set_trace()
+    time_lines = []
+    for index in range(line_num):
+        time_lines.append([])
+    for item in items:
+        item_id = item["value"][0]
+        if item_id in nonoverlap_id:
+            for time_item in time_lines[item_id]:
+                item_query = {'start': item["value"][1], 'end' : item["value"][2] }
+                item_match =  {'start': time_item["value"][1],  'end' : time_item["value"][2] }
+                try:
+                    assert(not overlaped(item_query, item_match) )
+                except:
+                    print('Error: time_line_overlaped: item_query = {item_query}; item_match= {item_match}'.format(item_query=item_query,item_match=item_match) )
+                    exit(0)
+        time_lines[item_id].append(item)
+        
+
 
 def main(fileroot,filenames,filelines,actions,colors,outputfile = 'data/outputdata/outputlog_test.json'):
     outputinfo = []
@@ -45,6 +78,11 @@ def main(fileroot,filenames,filelines,actions,colors,outputfile = 'data/outputda
     tmp = 1
     with open(outputfile,'w') as outfile:
         print_outinfo(outputinfo ,minstart , outfile)
+    with open(outputfile,'r') as jsonfile:
+        jsonstr = ''
+        for line in jsonfile.readlines():
+            jsonstr += line.strip()    
+        valid_json(jsonstr)
 
 if __name__ == "__main__":
     fileroot = 'data/inputlog/'
